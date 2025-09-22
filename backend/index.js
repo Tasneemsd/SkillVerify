5f4d00f0b3fe51dd4e537c10eca669358a02ea10
tasneemsd <tasneembanu2005@gmail.com>
2025-09-22 21:33:24 +0530

    backend

--- backend/index.js
+++ backend/index.js
@@ -19,7 +19,6 @@ seedCourses();
 const app = express();
 app.use(cors());
 app.use(express.json({ limit: '10mb' }));
-const cors = require("cors");
 
 app.use(cors({
   origin: ["https://skill-verify.vercel.app/"], // your frontend domain
